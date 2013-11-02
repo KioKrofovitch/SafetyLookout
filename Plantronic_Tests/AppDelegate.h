c09a6d16e002feee88cb12623eeeeb4b94fbe9d7
Royce Quintana <roycequintana@engr2gw-4-135-dhcp.int.colorado.edu>
2013-11-01 21:54:12 -0600

    Initial Commit

---
+++ Plantronic_Tests/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Plantronic_Tests
+//
+//  Created by Royce Quintana on 11/1/13.
+//  Copyright (c) 2013 Royce Quintana. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Plantronic_Tests/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Plantronic_Tests
+//
+//  Created by Royce Quintana on 11/1/13.
+//  Copyright (c) 2013 Royce Quintana. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
